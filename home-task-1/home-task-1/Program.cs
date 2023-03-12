b8720b50f8437bcab87336a609a713fecd9c0771
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-03-12 15:48:21 +0400

    first commit

---
+++ home-task-1/home-task-1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] currencies = new string[] { "USD", "RUB", "TRY" };
+        double[] currenciesWorth = new double[] { 1.7, 0.023, 0.18 };
+
+        while (true)
+        {
+            Console.WriteLine("Komandalar :");
+            Console.WriteLine("/show-recent-currency-currenciesWorth");
+            Console.WriteLine("/find-currency-rate-by-code");
+            Console.WriteLine("/calculate-amount-by-currecy");
+            Console.WriteLine("/exit");
+
+            string command = Console.ReadLine()!;
+
+            if (command == "/show-recent-currency-currenciesWorth")
+            {
+                int i = 0;
+
+                while (i < currencies.Length)
+                {
+                    Console.WriteLine(currencies[i] + currenciesWorth[i]);
+                    i++;
+
+                }
+            }
+            else if (command == "/find-currency-rate-by-code")
+            {
+                Console.Write("Məzənnənin alpha3 kodu daxil edin: ");
+
+                string currencyCode = Console.ReadLine()!;
+
+                int index = -1;
+                int i = 0;
+
+                while (i < currencies.Length)
+                {
+                    if (currencies[i] == currencyCode)
+                    {
+                        index = i;
+                        break;
+                    }
+                    i++;
+                }
+                if (index == -1)
+                {
+                    Console.WriteLine("Belə məzənnə yoxdur.");
+                }
+
+                else
+                {
+                    Console.WriteLine(currencyCode + currenciesWorth[index]);
+                }
+            }
+            else if (command == "/calculate-amount-by-currency")
+            {
+                Console.Write("Məbləği daxil edin: ");
+
+                double amount = double.Parse(Console.ReadLine()!);
+
+                Console.Write("Məzənnənin alpha3 kodu daxil edin: ");
+
+                string currencyCode = Console.ReadLine()!;
+
+                int index = -1;
+                int i = 0;
+
+                while (i < currencies.Length)
+                {
+                    if (currencies[i] == currencyCode)
+                    {
+                        index = i;
+                        break;
+                    }
+                    i++;
+                }
+
+                if (index == -1)
+                {
+                    Console.WriteLine("Not found");
+
+                }
+
+                else
+                {
+                    double total = amount * currenciesWorth[index];
+                    Console.WriteLine("Nəticə: " + total);
+
+                }
+            }
+
+            else if (command == "/exit")
+            {
+                Console.WriteLine("Programdan çıxılır.");
+                break;
+
+            }
+            else
+            {
+                Console.WriteLine("Düzgün komanda daxil edilmədi.");
+
+            }
+        }
+    }
+}
